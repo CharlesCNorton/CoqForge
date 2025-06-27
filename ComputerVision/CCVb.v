3fc78f1a5867e85122f9307c9da5952a5ff6d0b6
CharlesCNorton <135471798+CharlesCNorton@users.noreply.github.com>
2025-06-27 12:01:20 -0400

    Update CCVb.v

--- ComputerVision/CCVb.v
+++ ComputerVision/CCVb.v
@@ -159,3 +159,273 @@ Lemma bounded_to_simple_spec : forall img c,
 Proof.
   reflexivity.
 Qed.
+
+(** * Section 2: Adjacency Theory
+    
+    This section defines adjacency relations for pixels and establishes their
+    fundamental properties. We focus on 4-connectivity and 8-connectivity,
+    the two most common adjacency relations in image processing. *)
+
+(** ** 2.1 4-Connectivity (Edge Adjacency)
+    
+    Two pixels are 4-adjacent if they share an edge, i.e., they differ
+    by exactly 1 in either x or y coordinate (but not both).
+    
+    This gives each pixel up to 4 neighbors:
+    - (x-1, y) left
+    - (x+1, y) right  
+    - (x, y-1) up
+    - (x, y+1) down *)
+
+Definition adjacent_4 (c1 c2 : coord) : bool :=
+  let x1 := coord_x c1 in
+  let y1 := coord_y c1 in
+  let x2 := coord_x c2 in
+  let y2 := coord_y c2 in
+  match Nat.eqb x1 x2 with
+  | true => Nat.eqb (abs_diff y1 y2) 1  (* same x, differ by 1 in y *)
+  | false => andb (Nat.eqb y1 y2) (Nat.eqb (abs_diff x1 x2) 1)  (* same y, differ by 1 in x *)
+  end.
+
+(** ** 2.2 8-Connectivity (Edge or Corner Adjacency)
+    
+    Two pixels are 8-adjacent if they share an edge or corner, i.e., they differ
+    by at most 1 in both x and y coordinates (but not identical).
+    
+    This gives each pixel up to 8 neighbors, including diagonals. *)
+
+Definition adjacent_8 (c1 c2 : coord) : bool :=
+  let x1 := coord_x c1 in
+  let y1 := coord_y c1 in
+  let x2 := coord_x c2 in
+  let y2 := coord_y c2 in
+  let dx := abs_diff x1 x2 in
+  let dy := abs_diff y1 y2 in
+  andb (andb (Nat.leb dx 1) (Nat.leb dy 1)) 
+       (negb (andb (Nat.eqb dx 0) (Nat.eqb dy 0))).
+
+(** ** 2.3 Basic Tests for Adjacency *)
+
+Example test_adj4_horiz : adjacent_4 (0, 0) (1, 0) = true. 
+Proof. reflexivity. Qed.
+
+Example test_adj4_vert : adjacent_4 (0, 0) (0, 1) = true. 
+Proof. reflexivity. Qed.
+
+Example test_adj4_diag : adjacent_4 (0, 0) (1, 1) = false. 
+Proof. reflexivity. Qed.
+
+Example test_adj4_self : adjacent_4 (0, 0) (0, 0) = false. 
+Proof. reflexivity. Qed.
+
+Example test_adj8_horiz : adjacent_8 (0, 0) (1, 0) = true. 
+Proof. reflexivity. Qed.
+
+Example test_adj8_diag : adjacent_8 (0, 0) (1, 1) = true. 
+Proof. reflexivity. Qed.
+
+Example test_adj8_self : adjacent_8 (0, 0) (0, 0) = false. 
+Proof. reflexivity. Qed.
+
+Example test_adj8_far : adjacent_8 (0, 0) (2, 0) = false. 
+Proof. reflexivity. Qed.
+
+(** ** 2.4 Fundamental Properties of Adjacency *)
+
+(** 4-adjacency is symmetric *)
+Lemma adjacent_4_sym : forall c1 c2, 
+  adjacent_4 c1 c2 = adjacent_4 c2 c1.
+Proof.
+  intros [x1 y1] [x2 y2].
+  unfold adjacent_4; simpl.
+  rewrite Nat.eqb_sym, (Nat.eqb_sym y1 y2).
+  rewrite !abs_diff_sym.
+  destruct (Nat.eqb x2 x1); reflexivity.
+Qed.
+
+(** 8-adjacency is symmetric *)
+Lemma adjacent_8_sym : forall c1 c2,
+  adjacent_8 c1 c2 = adjacent_8 c2 c1.
+Proof.
+  intros [x1 y1] [x2 y2].
+  unfold adjacent_8; simpl.
+  rewrite !abs_diff_sym.
+  reflexivity.
+Qed.
+
+(** Neither adjacency relation is reflexive *)
+Lemma adjacent_4_irrefl : forall c,
+  adjacent_4 c c = false.
+Proof.
+  intros [x y].
+  unfold adjacent_4; simpl.
+  rewrite Nat.eqb_refl.
+  unfold abs_diff.
+  rewrite Nat.leb_refl, Nat.sub_diag.
+  reflexivity.
+Qed.
+
+Lemma adjacent_8_irrefl : forall c,
+  adjacent_8 c c = false.
+Proof.
+  intros [x y].
+  unfold adjacent_8; simpl.
+  unfold abs_diff.
+  rewrite !Nat.leb_refl, !Nat.sub_diag.
+  simpl.
+  rewrite !Nat.eqb_refl.
+  reflexivity.
+Qed.
+
+(** ** 2.5 Relationship Between 4 and 8 Adjacency *)
+
+(** 4-adjacency implies 8-adjacency *)
+Theorem adjacent_4_implies_8 : forall c1 c2,
+  adjacent_4 c1 c2 = true -> adjacent_8 c1 c2 = true.
+Proof.
+  intros [x1 y1] [x2 y2] H4.
+  unfold adjacent_4 in H4; simpl in H4.
+  unfold adjacent_8; simpl.
+  destruct (Nat.eqb x1 x2) eqn:Heqx.
+  - (* Same x coordinate *)
+    apply Nat.eqb_eq in Heqx; subst x2.
+    assert (abs_diff x1 x1 = 0).
+    { unfold abs_diff. rewrite Nat.leb_refl. lia. }
+    rewrite H. simpl.
+    assert (abs_diff y1 y2 = 1).
+    { apply Nat.eqb_eq. exact H4. }
+    rewrite H0. simpl.
+    reflexivity.
+  - (* Different x, must have same y *)
+    apply andb_prop in H4. destruct H4 as [Heqy H1].
+    apply Nat.eqb_eq in Heqy; subst y2.
+    assert (abs_diff y1 y1 = 0).
+    { unfold abs_diff. rewrite Nat.leb_refl. lia. }
+    rewrite H. simpl.
+    apply Nat.eqb_eq in H1.
+    rewrite H1. simpl.
+    rewrite Nat.eqb_refl. simpl.
+    apply Nat.eqb_neq in Heqx.
+    destruct (Nat.eq_dec x1 x2); [contradiction|].
+    destruct (Nat.eqb (abs_diff x1 x2) 0) eqn:Habs.
+    + apply Nat.eqb_eq in Habs.
+      unfold abs_diff in Habs.
+      destruct (Nat.leb x1 x2) eqn:Hle.
+      * apply Nat.leb_le in Hle. lia.
+      * apply Nat.leb_nle in Hle. lia.
+    + reflexivity.
+Qed.
+
+(** The converse is not true: 8-adjacency does not imply 4-adjacency *)
+Example adjacent_8_not_implies_4 : 
+  exists c1 c2, adjacent_8 c1 c2 = true /\ adjacent_4 c1 c2 = false.
+Proof.
+  exists (0, 0), (1, 1).
+  split; reflexivity.
+Qed.
+
+(** ** 2.6 Adjacency Characterizations *)
+
+(** 4-adjacency can be characterized by unit distance in Manhattan metric *)
+Lemma adjacent_4_manhattan : forall c1 c2,
+  adjacent_4 c1 c2 = true <-> 
+  abs_diff (coord_x c1) (coord_x c2) + abs_diff (coord_y c1) (coord_y c2) = 1.
+Proof.
+  intros [x1 y1] [x2 y2].
+  unfold adjacent_4; simpl.
+  split.
+  - intros H.
+    destruct (Nat.eqb x1 x2) eqn:Hx.
+    + apply Nat.eqb_eq in Hx; subst.
+      apply Nat.eqb_eq in H.
+      rewrite H.
+      unfold abs_diff at 1.
+      rewrite Nat.leb_refl, Nat.sub_diag.
+      reflexivity.
+    + apply andb_prop in H.
+      destruct H as [Hy H1].
+      apply Nat.eqb_eq in Hy; subst.
+      apply Nat.eqb_eq in H1.
+      rewrite H1.
+      unfold abs_diff at 2.
+      rewrite Nat.leb_refl, Nat.sub_diag.
+      lia.
+  - intros H.
+    destruct (Nat.eqb x1 x2) eqn:Hx.
+    + apply Nat.eqb_eq in Hx; subst.
+      unfold abs_diff at 1 in H.
+      rewrite Nat.leb_refl, Nat.sub_diag in H.
+      simpl in H.
+      apply Nat.eqb_eq.
+      exact H.
+    + destruct (Nat.eqb y1 y2) eqn:Hy.
+      * apply Nat.eqb_eq in Hy; subst.
+        unfold abs_diff at 2 in H.
+        rewrite Nat.leb_refl, Nat.sub_diag in H.
+        rewrite Nat.add_0_r in H.
+        rewrite Hy.
+        simpl.
+        apply Nat.eqb_eq.
+        exact H.
+      * (* x1 ≠ x2 and y1 ≠ y2 but sum = 1, impossible *)
+        apply Nat.eqb_neq in Hx, Hy.
+        assert (abs_diff x1 x2 > 0).
+        { unfold abs_diff. 
+          destruct (Nat.leb x1 x2) eqn:Hle.
+          - apply Nat.leb_le in Hle. lia.
+          - apply Nat.leb_nle in Hle. lia. }
+        assert (abs_diff y1 y2 > 0).
+        { unfold abs_diff. 
+          destruct (Nat.leb y1 y2) eqn:Hle.
+          - apply Nat.leb_le in Hle. lia.
+          - apply Nat.leb_nle in Hle. lia. }
+        lia.
+Qed.
+
+(** 8-adjacency can be characterized by Chebyshev distance *)
+Lemma adjacent_8_chebyshev : forall c1 c2,
+  adjacent_8 c1 c2 = true <-> 
+  max (abs_diff (coord_x c1) (coord_x c2)) (abs_diff (coord_y c1) (coord_y c2)) = 1.
+Proof.
+  intros [x1 y1] [x2 y2].
+  unfold adjacent_8; simpl.
+  split.
+  - intros H.
+    apply andb_prop in H.
+    destruct H as [H1 H2].
+    apply andb_prop in H1.
+    destruct H1 as [Hdx Hdy].
+    apply Nat.leb_le in Hdx, Hdy.
+    apply negb_true_iff in H2.
+    apply andb_false_iff in H2.
+    assert (abs_diff x1 x2 <= 1) by exact Hdx.
+    assert (abs_diff y1 y2 <= 1) by exact Hdy.
+    assert (abs_diff x1 x2 > 0 \/ abs_diff y1 y2 > 0).
+    { destruct H2 as [H2 | H2].
+      - apply Nat.eqb_neq in H2. left. lia.
+      - apply Nat.eqb_neq in H2. right. lia. }
+    destruct H1 as [Hx | Hy].
+    + destruct (abs_diff x1 x2) eqn:E; [lia|].
+      destruct n; [|lia].
+      destruct (abs_diff y1 y2); lia.
+    + destruct (abs_diff y1 y2) eqn:E; [lia|].
+      destruct n; [|lia].
+      destruct (abs_diff x1 x2); lia.
+  - intros H.
+    apply andb_true_intro.
+    split.
+    + apply andb_true_intro.
+      split; apply Nat.leb_le; lia.
+    + apply negb_true_iff.
+      apply andb_false_iff.
+      unfold max in H.
+      destruct (abs_diff x1 x2 <=? abs_diff y1 y2) eqn:Hle.
+      * apply Nat.leb_le in Hle.
+        right.
+        apply Nat.eqb_neq.
+        lia.
+      * apply Nat.leb_nle in Hle.
+        left.
+        apply Nat.eqb_neq.
+        lia.
+Qed.
